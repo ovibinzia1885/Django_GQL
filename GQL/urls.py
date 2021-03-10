5ac1b6da34edba8779ccfd9cf29de1f27f058225
ovibinzia1885 <nazmul35-1885@diu.edu.bd>
2021-03-10 16:50:24 +0600

    fisrt gql

---
+++ GQL/urls.py
@@ -0,0 +1,11 @@
+
+from django.contrib import admin
+from django.urls import path
+from graphene_django.views import GraphQLView
+from GQL.schema import schema
+from graphql_jwt.decorators import jwt_cookie
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('graphql/',jwt_cookie( GraphQLView.as_view(graphiql=True))),
+]
---
+++ api/schema.py
@@ -0,0 +1,142 @@
+import graphene
+from graphene import relay
+from graphene_django.types import DjangoObjectType
+from api.models import Movie,Director
+import graphql_jwt
+from graphene_django.filter import DjangoFilterConnectionField
+from graphql_jwt.decorators import login_required
+from graphql_relay import from_global_id
+
+class MovieType(DjangoObjectType):
+    class Meta:
+        model=Movie
+    movie_age=graphene.String()
+
+    def resolve_movie_age(self, info):
+
+        return "old movie" if self.year <2000 else "newmovie"
+
+
+class DirectorType(DjangoObjectType):
+    class Meta:
+        model=Director
+
+# just for rely implementation
+class MovieNode(DjangoObjectType):
+    class Meta:
+        model=Movie
+        filter_fields={
+            'title':['exact','icontains','istartswith'],
+            'year':['exact',]
+        }
+
+        interfaces=(relay.Node,)
+
+class Query(graphene.ObjectType):
+    # all_movies=graphene.List(MovieType)
+    all_movies=DjangoFilterConnectionField(MovieNode)
+    # movie=graphene.Field(MovieType,id=graphene.Int())
+    # movies=graphene.List(MovieType,id=graphene.Int(),title=graphene.String())
+    movie=relay.Node.Field(MovieNode)
+    all_directors=graphene.List(DirectorType)
+
+    # @login_required
+    # def resolve_all_movies(self,info,**kwargs):
+    #     # user=info.context.user
+    #     # if not user.is_authenticated:
+    #     #     raise Exception('Auth credentials were not provied')
+    #
+    #     return Movie.objects.all()
+    def resolve_all_directors(self,info,**kwargs):
+        return Director.objects.all()
+
+    # def resolve_movie(self,info,**kwargs):
+    #     id=kwargs.get('id')
+    #     if id is not None:
+    #         return Movie.objects.get(pk=id)
+    #
+    #     # return Movie.objects.all()
+    #     return None
+
+
+    # def resolve_movies(self,info,**kwargs):
+    #     id = kwargs.get('id')
+    #     title=kwargs.get('title')
+    #     if id is not None:
+    #         return [Movie.objects.get(pk=id)]
+    #     if title is not None:
+    #         return [Movie.objects.get(title=title)]
+    #
+    #     return Movie.objects.all()
+
+
+class MovieCreateMutation(graphene.Mutation):
+    class Arguments:
+        title=graphene.String(required=True)
+        year=graphene.Int(required=True)
+
+    movie=graphene.Field(MovieType)
+
+
+    def mutate(self,info,title,year):
+        movie=Movie.objects.create(title=title,year=year)
+        return MovieCreateMutation(movie=movie)
+
+class MovieUpdateMutation(graphene.Mutation):
+    class Arguments:
+        title=graphene.String()
+        year=graphene.Int()
+        id=graphene.ID(required=True)
+
+    movie=graphene.Field(MovieType)
+
+    def mutate(self,info,id,title,year):
+        movie=Movie.objects.get(pk=id)
+        if title is not None:
+            movie.title=title
+        if year is not None:
+            movie.year=year
+        movie.save()
+        return MovieUpdateMutation(movie=movie)
+
+class MovieUpdateMutationRelay(relay.ClientIDMutation):
+    class Input:
+        title = graphene.String()
+        id = graphene.ID(required=True)
+
+    movie = graphene.Field(MovieType)
+
+    @classmethod
+    def mutate_and_get_payload(cls,root, info, id, title):
+        movie = Movie.objects.get(pk=from_global_id(id)[1])
+        if title is not None:
+            movie.title = title
+        movie.save()
+        return MovieUpdateMutationRelay(movie=movie)
+
+
+class MovieDeleteMutation(graphene.Mutation):
+    class Arguments:
+        id=graphene.ID(required=True)
+    movie=graphene.Field(MovieType)
+
+    def mutate(self,info,id):
+        movie=Movie.objects.get(pk=id)
+        movie.delete()
+        return MovieDeleteMutation(movie=None)
+
+
+
+class Mutation:
+    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
+    verfiy_token=graphql_jwt.Verify.Field( )
+    create_movie=MovieCreateMutation.Field()
+    update_movie=MovieUpdateMutation.Field()
+    update_movie_relay=MovieUpdateMutationRelay.Field()
+    delete_movie=MovieDeleteMutation.Field()
+
+
+
+
+
+
